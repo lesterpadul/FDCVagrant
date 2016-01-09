cacedab23cc601a73dd2f329ae330edf12f81b90
Lester Ag Padul <padulleste@gmail.com>
2016-01-09 09:36:59 +0800

    FDCVagrant : change configuration

--- provision/setup_cnf.sh
+++ provision/setup_cnf.sh
@@ -1,13 +1,13 @@
 echo "Transferring my.cnf file"
-cat /vagrant/conf/my.cnf
-cp /vagrant/conf/my.cnf /etc/my.cnf
+cat /vagrant/conf/mysql/my.cnf
+cp /vagrant/conf/mysql/my.cnf /etc/my.cnf
 sudo service mysqld restart
 
 echo "Transferring vhost configuration"
-cat /vagrant/conf/vhost.conf
-cp /vagrant/conf/vhost.conf /etc/httpd/conf.d/vhost_ssl.conf
+cat /vagrant/conf/vhost/vhost.conf
+cp /vagrant/conf/vhost/vhost.conf /etc/httpd/conf.d/vhost_ssl.conf
 
 echo "Transferring vhost_ssl configuration"
 sudo yum -y install mod_ssl
-cat /vagrant/conf/vhost_ssl.conf
-cp /vagrant/conf/vhost_ssl.conf /etc/httpd/conf.d/vhost_ssl.conf
+cat /vagrant/conf/vhost/vhost_ssl.conf
+cp /vagrant/conf/vhost/vhost_ssl.conf /etc/httpd/conf.d/vhost_ssl.conf
