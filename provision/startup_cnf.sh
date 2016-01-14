55b146f5624e3138546c1a2a6cc548b90dc616c7
Lester Ag Padul <padulleste@gmail.com>
2016-01-14 19:15:01 +0800

    FDCVagrant : fix nodejs startup

--- provision/startup_cnf.sh
+++ provision/startup_cnf.sh
@@ -3,4 +3,4 @@ sudo service httpd stop
 sudo service httpd start
 
 echo "Starting NodeJS"
-cd /var/www/nativecamp-njs && nohup supervisor server.js
+cd /var/www/nativecamp-njs && nohup supervisor server.js &
