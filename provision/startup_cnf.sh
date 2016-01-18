9904f09a731750eda269f277100b7b14d0383687
Lester Ag Padul <padulleste@gmail.com>
2016-01-18 11:29:20 +0800

    FDCVagrant : enable tmp/webroot and fix nodejs

--- provision/startup_cnf.sh
+++ provision/startup_cnf.sh
@@ -4,10 +4,11 @@ sudo service httpd start
 
 echo "Starting NodeJS"
 cd /vagrant/workspace/nativecamp-njs && node server.js
+echo -ne '\n'
 
 echo "Enabling permissions"
-cd /vagrant/workspace/nativecamp && chmod -R 777 user/webroot
-cd /vagrant/workspace/nativecamp && chmod -R 777 teacher/webroot
-cd /vagrant/workspace/nativecamp && chmod -R 777 admin/webroot
-cd /vagrant/workspace/nativecamp && chmod -R 777 instructor/webroot
-cd /vagrant/workspace/nativecamp && chmod -R 777 app/webroot
+cd /vagrant/workspace/nativecamp && chmod -R 777 user/webroot && chmod -R 777 user/tmp
+cd /vagrant/workspace/nativecamp && chmod -R 777 teacher/webroot && chmod -R 777 teacher/tmp
+cd /vagrant/workspace/nativecamp && chmod -R 777 admin/webroot && chmod -R 777 admin/tmp
+cd /vagrant/workspace/nativecamp && chmod -R 777 instructor/webroot && chmod -R 777 instructor/tmp
+cd /vagrant/workspace/nativecamp && chmod -R 777 app/webroot && chmod -R 777 app/tmp
