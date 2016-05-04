a383e1fe272654b19225a32bf7cd019e756d4636
lesterpadul <padullester@gmail.com>
2016-05-04 08:26:29 +0800

    FDCVagrant: upgrade nodejs

--- provision/setup_cnf.sh
+++ provision/setup_cnf.sh
@@ -2,3 +2,8 @@ echo "Transferring my.cnf file"
 cat /vagrant/conf/mysql/my.cnf
 cp /vagrant/conf/mysql/my.cnf /etc/my.cnf
 sudo service mysqld restart
+
+echo "Upgrading NODEJS"
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
