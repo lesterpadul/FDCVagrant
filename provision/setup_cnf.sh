3d3e5d0d4b6e537095a4badb7ef16457b6372cfd
lesterpadul <padullester@gmail.com>
2016-05-04 09:47:20 +0800

    FDCVagrant: install nodejs

--- provision/setup_cnf.sh
+++ provision/setup_cnf.sh
@@ -3,6 +3,15 @@ cat /vagrant/conf/mysql/my.cnf
 cp /vagrant/conf/mysql/my.cnf /etc/my.cnf
 sudo service mysqld restart
 
+echo "Re-installing NODEJS"
+cd /usr/src
+sudo wget http://nodejs.org/dist/v0.10.4/node-v0.10.4.tar.gz
+sudo tar zxf node-v0.10.4.tar.gz
+cd node-v0.10.4
+sudo ./configure
+sudo make
+sudo make install
+
 echo "Upgrading NODEJS"
 sudo npm cache clean -f
 sudo npm install -g n
