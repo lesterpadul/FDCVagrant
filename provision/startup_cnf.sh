73fa2445e260c0f720d35db665cc7229aed25b81
lesterpadul <padullester@gmail.com>
2016-06-24 14:47:31 +0800

    FDCVagrant: process nohup

--- provision/startup_cnf.sh
+++ provision/startup_cnf.sh
@@ -2,8 +2,12 @@ echo "Restart HTTPD"
 sudo service httpd stop
 sudo service httpd start
 
+echo "Killing PORTS"
+sudo fuser -k 3000/tcp
+sudo fuser -k 3001/tcp
+
 echo "Starting NodeJSv2"
-cd /var/www/NativeCamp-NJSv2 && node server.js > /dev/null &
+nohup /var/www/NativeCamp-NJSv2 && node server.js > /dev/null &
 
 echo "Starting NodeJS Broadcast"
-cd /var/www/NativeCamp-NJSBroadcast && node server.js > /dev/null &
+nohup /var/www/NativeCamp-NJSBroadcast && node server.js > /dev/null &
