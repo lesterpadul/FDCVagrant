170631dee71ecf6cbec327ce0c6c88ec8d2cf63e
lesterpadul <padullester@gmail.com>
2016-06-24 15:43:53 +0800

    FDCVagrant: add port killer

--- provision/startup_cnf.sh
+++ provision/startup_cnf.sh
@@ -2,6 +2,10 @@ echo "Restart HTTPD"
 sudo service httpd stop
 sudo service httpd start
 
+echo "Killing Ports"
+sudo fuser -k 3000/tcp
+sudo fuser -k 3001/tcp
+
 echo "Starting NodeJSv2"
 cd /var/www/NativeCamp-NJSv2 && forever start signaling.json
 
