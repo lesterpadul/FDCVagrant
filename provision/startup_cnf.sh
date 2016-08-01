0f64086a27f07612238aeb96ab2a4c61fb5c8b20
lesterpadul <padullester@gmail.com>
2016-08-01 16:48:52 +0800

    FDCVagrant: ignore outputs

--- provision/startup_cnf.sh
+++ provision/startup_cnf.sh
@@ -7,7 +7,9 @@ sudo fuser -k 3000/tcp
 sudo fuser -k 3001/tcp
 
 echo "Starting NodeJSv2"
-cd /var/www/NativeCamp-NJSv2 && nohup node server.js
+cd /var/www/NativeCamp-NJSv2 && nohup node server.js 2>&1 &
 
 echo "Starting NodeJS Broadcast"
-cd /var/www/NativeCamp-NJSBroadcast && nohup node server.js
+cd /var/www/NativeCamp-NJSBroadcast && nohup node server.js 2>&1 &
+
+echo "Start up complete!";
