1b87814867b708496ca13260950e5741a47640e8
lesterpadul <padullester@gmail.com>
2016-12-02 10:05:58 +0800

    FDCVagrant: add missing files

---
+++ conf/nativecamp/cards.php
@@ -0,0 +1,9 @@
+<?php
+# --- CREDIT CARD DATA ---
+# TEST CLIENTIP
+$config['ZEUS_clientip'] = 0123456789;
+$config['AXES_clientip'] = 0123456789;
+
+# --- NEXMO SMS DATA ---
+$config['nexmo_api_key'] = '11111111';
+$config['nexmo_api_secret'] = '11111111';
---
+++ conf/nativecamp/memcached.php
@@ -0,0 +1,7 @@
+<?php 
+
+// configure memcached
+$config['memcached'] = array(
+	'server' => 'localhost',
+	'port' => 11211
+);
